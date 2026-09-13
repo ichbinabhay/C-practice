4dc6ea1778af7bf89884f8701fe6ec78fbfd2fcf
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-13 12:07:49 +0530

    added check3.c

---
+++ happy/check2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h> 
+int main() {
+    int isSunday=1;
+    int isSnowing=1;
+    printf("%d \n", isSunday && isSnowing);
+    return 0;
+} //here the program is having sunday as true(1) and snowing is also true(1) so final result is also is true 
+ 
