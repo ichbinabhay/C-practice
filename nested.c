3c2de85f5fbc13281cb802667fcfbaa43cd531a0
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-14 11:19:55 +0530

    added nested.c

---
+++ nested.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int number;
+    printf("enter number:");
+    scanf("%d",&number);
+    
+    if(number >=0){
+        printf("number is positive \n");
+        if(number%2==0) {
+            printf("number is even \n"); 
+
+        } else {
+            printf("number is odd \n");
+
+        }
+    } else {
+        printf("number is negative \n");
+    }
+    return 0;
+}
