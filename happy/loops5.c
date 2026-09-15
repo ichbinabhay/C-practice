7b1d80eba82b3e776a55ed8f5b894ce4d5a1d51d
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-15 09:15:27 +0530

    added loops5.c

---
+++ happy/loops5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    //increment operator
+    //++i (pre increment)
+    //i++ (post increment)
+    //--i (pre decrement)
+    //i-- (post decrement)
+    
+    int i=1;
+    printf("%d \n", ++i); //increase then use
+    printf("%d \n", i);
+    return 0;
+}
