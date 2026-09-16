901a4388459c1352a847afafe711df3451a61f46
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-16 21:57:36 +0530

    added  break3.c

---
+++ happy/break3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+    for(int i=1;i<=100;i++){
+        if(i==19){
+            break;
+        }
+        printf("%d \n",i);
+        
+
+        
+    }
+    printf("end");
+    return 0;
+
+
+    
+       
+    
+}
