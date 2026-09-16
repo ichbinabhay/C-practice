ba5b18a8022858f4304547685093aa341167e472
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-16 08:13:07 +0530

    added tableofnumber.c

---
+++ happy/tableofnumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int i;
+    printf("enter the number");
+    scanf("%d \n",&i);
+    for(int n=1;n<=10;n++){
+        printf("%d\n", i*n);
+    }
+    return 0;
+}
