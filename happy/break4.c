c2b620ddbe0e97b5081bada5c47cd96128abac2a
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-16 22:04:12 +0530

    added break4.c

---
+++ happy/break4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    for(int i=88;i<=100;i++){
+        if(i==96){
+            break;
+        }
+        printf("%d \n", i);
+        
+    }
+    printf("end");
+    return 0;
+}
