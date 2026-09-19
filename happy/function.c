94dbf93eba7b442f2d714c1a036e577563eef9f5
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-19 13:07:59 +0530

    added function.c

---
+++ happy/function.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+void printHello();
+void printGoodbye();
+
+int main(){
+    printHello();
+    printGoodbye();
+    return 0;
+}
+
+void printHello(){
+    printf("Hello!\n");
+    printf("I am abhay singh\n");
+}
+
+void printGoodbye(){
+    printf("Goodbye:)\n");
+}
