5573b6ba684f443a4cd692cb56182cab994a93c3
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-19 12:32:46 +0530

    added sumbtw.c

---
+++ happy/sumbtw.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int sum=0;
+    for(int i=5;i<=50;i++){
+        sum+=i;
+        
+
+        
+
+    }
+    printf("%d\n",sum);
+    return 0;
+}
