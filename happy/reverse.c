c9c6562a2d8b7b31abd7a39418ad1925cebc80f7
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-19 10:53:15 +0530

    added reverse.c

---
+++ happy/reverse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int i;
+    printf("enter number:");
+    scanf("%d",&i);
+
+    int product;
+    for(int n=10;n>=1;n--){
+        product=i*n;
+        printf("%d\n",product); 
+
+        
+    }
+    
+   
+    return 0;
+
+}
