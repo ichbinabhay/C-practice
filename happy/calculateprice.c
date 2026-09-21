9114ac4880690ffdcff7b47a857ca7eaaff36720
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-21 22:16:42 +0530

    added calculateprice

---
+++ happy/calculateprice.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+void calculateprice(float value);
+int main(){
+    float value=100.0;
+    calculateprice(value);
+    printf("value is: %f\n",value);
+    return 0;
+
+}
+void calculateprice(float value) {
+    value=value+(0.18*value);
+    printf("final price: %f\n",value);
+
+}
